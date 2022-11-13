3d9c04512f1c14aecba936a306888e322da58fea
Sergey Petrenko <Stevo98@mail.ru>
2022-11-13 14:18:11 +1000

    Corrected mistakes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        int count = 300;
+        int moneyAmount = 300;
         int refill = 2000;
-        int sum = count + refill;
-        int a = sum/100;
+        int sum = moneyAmount + refill;
+        int rublesFor100 = sum / 100;
         if (sum > 1000) {
-            System.out.println("Количество средств на счету (RUB) - " + sum + "; Сумма ваших бонусов(RUB) - " + a);
+            System.out.println("Количество средств на счету (RUB) - " + sum + "; Сумма ваших бонусов(RUB) - " + rublesFor100);
         } else {
-            System.out.println( "Количество средств на счету (RUB) - " + sum + "; В настоящий момент у вас нет бонусов :(");
+            System.out.println("Количество средств на счету (RUB) - " + sum + "; В настоящий момент у вас нет бонусов :(");
+        }
     }
-}
 }
